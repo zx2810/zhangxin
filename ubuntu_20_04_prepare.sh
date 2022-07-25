db78c5dd15d14743f85db7a97c3b4e3614f7c732
zhangxin <96939039+zx2810@users.noreply.github.com>
2022-07-25 19:49:26 +0800

    Update ubuntu_20_04_prepare.sh

--- ubuntu_20_04_prepare.sh
+++ ubuntu_20_04_prepare.sh
@@ -14,14 +14,19 @@ deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted 
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
 # 预发布软件源，不建议启用
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse' > /etc/apt/sources.list /etc/apt/sources.list
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse' > /etc/apt/sources.list
 source /etc/apt/sources.list
-sudo apt update && sudo apt upgrade
+sudo apt update
+# sudo apt update && sudo apt upgrade
 
 #Configure conda and pip.
 cd ~
 wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
 bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
+##If conda is not added to the environment
+# echo 'export PATH=~/anaconda3/bin:$PATH' >> .bashrc
+#source  .bashrc
+
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/conda-forge/
@@ -29,9 +34,6 @@ conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/msys2/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/bioconda/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/menpo/
 conda config --set show_channel_urls yes
-##If conda is not added to the environment
-# echo 'export PATH=~/anaconda3/bin:$PATH' >> .bashrc
-#source  .bashrc
 conda update conda
 conda install pip
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
