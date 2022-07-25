b7369b0f7900ea7d6e66c37512d4e6a1115c234f
zhangxin <96939039+zx2810@users.noreply.github.com>
2022-07-26 02:10:23 +0800

    Add files via upload

---
+++ jupyterlab_optimize.sh
@@ -0,0 +1,23 @@
+#%matplotlib widget
+pip install --upgrade jupyterlab jupyterlab-git
+pip install jupyterlab-github
+jupyter labextension install @jupyterlab/toc
+pip install jupyterlab_latex
+jupyter labextension install @mflevine/jupyterlab_html
+pip install jupyterlab-fasta
+pip install jupyterlab-geojson
+pip install jupyterlab-katex
+pip install jupyterlab-mathjax3
+pip install jupyterlab-vega2
+pip install jupyterlab-vega3
+pip install jupyter_bokeh
+pip install jupyterlab-drawio
+pip install jupyterlab_sql
+jupyter serverextension enable jupyterlab_sql --py --sys-prefix
+jupyter lab build
+pip install lckr-jupyterlab-variableinspector
+pip install "jupyterlab>=1.0" jupyterlab-dash==0.1.0a3
+jupyter labextension install nbgather
+pip install 'jupyterlab>=3.0.0,<4.0.0a0' jupyterlab-lsp
+jupyter labextension install jupyterlab-spreadsheet
+pip install "jupyterlab-kite>=2.0.2"
