0dae80c7f13ab937cf6b8bdb1ec3e6b0cbef2e45
zhangxin <96939039+zx2810@users.noreply.github.com>
2022-07-26 01:48:09 +0800

    Update jupyterlab_prepare.sh

--- jupyterlab_prepare.sh
+++ jupyterlab_prepare.sh
@@ -4,7 +4,15 @@
 cd ~
 conda install -c conda-forge jupyterlab
 jupyter notebook --generate-config
-echo 'c.NotebookApp.use_redirect_file = False' >> .jupyter/jupyter_notebook_config.py
+echo 'import webbrowser
+webbrowser.register('edge',None,webbrowser.GenericBrowser('/usr/bin/edge'))
+c.NotebookApp.browser = 'edge'
+c.NotebookApp.use_redirect_file = Fals
+c.NotebookApp.allow_root = True' >> .jupyter/jupyter_notebook_config.py
 echo "export BROWSER='/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'" >> .bashrc
 source .bashrc
 jupyter lab
+# jupyter-lab --allow-root 
+#Instead, copy the link directly from the command line.
+
+#End
